b2fe03906ff3fd27547cc648daeaa698f1fd96bf
vtimoshenko <vtimoshenko@pktbcki.ru>
2016-06-18 20:53:08 +0300

    Дополнен интерфейс (расскажи о себе) Добавлен джавадок

--- src/com/rzd/pktb/SPSGame/gameSPS.java
+++ src/com/rzd/pktb/SPSGame/gameSPS.java
@@ -1,22 +1,49 @@
 package com.rzd.pktb.SPSGame;
 
 /**
+ * Описание игры камень, ножницы бумага
+ *
  * Created by SimpleUser on 18.06.2016.
  */
 public class gameSPS {
+    /**
+     * Камень
+     */
     public static int STONE = 0;
+    /**
+     * Бумага
+     */
     public static int PAPER = 1;
+    /**
+     * Ножницы
+     */
     public static int SCISSORS = 2;
-
+    /**
+     * Выиграл
+     */
     public static int WON = 1;
+    /**
+     * Ничья
+     */
     public static int NONE = 0;
+    /**
+     * Проиграл
+     */
     public static int LOSS = -1;
-
+    /**
+     * Матрица определения победителя
+     */
     private static int[][] matrix = {
             {NONE,LOSS,WON},
             {WON,NONE,LOSS},
             {LOSS,WON,NONE}
     };
+    /**
+     * Сыграть кон. Принимаем знаки от двух игроков.
+     * @param player1 Что показал первый игрок
+     * @param player2 Что показал второй игрок
+     * @return Результат для первого игрока (проиграл/выиграл/ничья)
+     */
     public static int fight(int player1, int player2) {
         return matrix[player1][player2];
     }
--- src/com/rzd/pktb/SPSGame/playerSPS.java
+++ src/com/rzd/pktb/SPSGame/playerSPS.java
@@ -4,6 +4,20 @@ package com.rzd.pktb.SPSGame;
  * Created by SimpleUser on 18.06.2016.
  */
 public interface playerSPS {
+    /**
+     * Участвовать в игре. Показать один из знаков
+     * @return
+     */
     int fight();
+    /**
+     * Указать игроку результат предыдущего кона.
+     * @param result Результат игры предыдущего кона (ничья/виграл/проиграл)
+     * @param enemyStep Указать какой знак показал противник (камень/ножницы/бумага)
+     */
     void result(int result, int enemyStep);
+    /**
+     * Информация об игроке
+     * @return строка описание класса-игрока и/или разработчика
+     */
+    String about();
 }
